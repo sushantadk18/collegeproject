0858d264236a40c1288c782da5ea0457f88787bd
Sushant Adhikari <sushantadk18@gmail.com>
2023-12-08 07:29:58 +0545

    second commit

--- admin/users.php
+++ admin/users.php
@@ -95,7 +95,7 @@ else
                         <td><?php echo $d ?></td>
                         <td><?php echo $e ?></td>
                         <td><a href="edit.php?id=<?php echo $a; ?>&fname=<?php echo $b ?> &lname=<?php echo $c ?>&username=<?php echo $d ?>&password=<?php echo $e ?>  ">Edit</a></td>
-                        <td><a href="delete.php">Delete</a></td>
+                        <td><a href="delete.php?Del=<?php echo $a ; ?>">Delete</a></td>
 
                         
                     </tr>
